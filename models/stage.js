2df27e1c584caf8c61e65975ee36f6897bdd9f7b
haydenn+1-at-933632846737 <haydenn@scigrp.com>
2023-05-19 23:51:23 -0400

    activity 1 complete

--- models/stage.js
+++ models/stage.js
@@ -9,25 +9,38 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    // define association here
+    static associate({ Event, StageEvent, SetTime }) {
+      // events
+      Stage.belongsToMany(Event, {
+        foreignKey: "stage_id",
+        as: "events",
+        through: StageEvent
+      })
+
+      // set times
+      Stage.hasMany(SetTime, {
+        foreignKey: "stage_id",
+        as: "set_times"
+      })
     }
   }
-  Stage.init({
-    stage_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    stage_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    }
-  }, {
-    sequelize,
-    modelName: 'Stage',
-    tableName: 'stages',
-    timestamps: false
-  })
-  return Stage
+}
+Stage.init({
+  stage_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  stage_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  modelName: 'Stage',
+  tableName: 'stages',
+  timestamps: false
+})
+return Stage
 }
